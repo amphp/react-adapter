6a7cd27ea6f784b8b701ad50cefc500e31c2bc89
Niklas Keller <me@kelunik.com>
2017-03-29 12:50:53 +0200

    Add temp timeout 'fix'

--- test/AdapterTest.php
+++ test/AdapterTest.php
@@ -11,6 +11,9 @@ class AdapterTest extends AbstractLoopTest {
     public function setUp() {
         Loop::set(new NativeDriver);
 
+        // FIXME: Remove once https://github.com/reactphp/event-loop/pull/92 is merged
+        \define("HHVM_VERSION", true);
+
         parent::setUp();
     }
 
